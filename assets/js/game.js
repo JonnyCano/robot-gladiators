8125ac481589d8508c67abe18a9c870c4fc7441e
Jonathan Cano <jcano528@gmail.com>
2021-06-25 12:17:47 -0500

    validate user input

--- assets/js/game.js
+++ assets/js/game.js
@@ -70,7 +70,7 @@ var fight = function(enemy) {
 
 // function to start a new game
 var startGame = function() {
-    //debugger;
+
     // reset player stats
     playerInfo.reset();
 
@@ -79,7 +79,6 @@ var startGame = function() {
             window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
             var pickedEnemyObj = enemyInfo[i];
             pickedEnemyObj.health = randomNumber(40, 60);
-            //debugger;
             fight(pickedEnemyObj);
 
             // if we are not at the last enemy in the array
@@ -132,8 +131,19 @@ var randomNumber = function(min, max) {
     return value;
 }
 
+var getPlayerName = function() {
+    var name = "";
+
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+
+    console.log("Your robot's name is " + name);
+    return name;
+}
+
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
