4fb4ce915fd0aec552339b533dfb8fa7686a1234
Jonathan Cano <jcano528@gmail.com>
2021-07-01 19:24:14 -0500

    final check

--- assets/js/game.js
+++ assets/js/game.js
@@ -141,38 +141,6 @@ var startGame = function() {
     endGame();
 }
 
-// function to end the entire game
-var endGame = function() {
-    window.alert("The game has now ended. Let's see how you did!");
-
-    // check localStorage for high score, if it's not there, use 0
-    var highScore = localStorage.getItem("highscore");
-    if (highScore === null) {
-        highscore = 0;
-    }
-    // if player has more monr=ey than the high score, player has new high score!
-    if (playerInfo.money > highScore) {
-        localStorage.setItem("highscore", playerInfo.money);
-        localStorage.setItem("name", playerInfo.name);
-
-        alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
-    }
-    else {
-        alert(playerInfo.name + " did not beat the high score of " + highscore + ". Maybe next time!");
-    }
-
-    // ask player if they'd like to play again
-    var playAgainConfirm = window.confirm("Would you like to play again?");
-
-    if (playAgainConfirm) {
-        // restart the game
-        startGame();
-    }
-    else {
-        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
-    }
-};
-
 // function to generate a random numeric value
 var randomNumber = function(min, max) {
     var value = Math.floor(Math.random() * (max - min + 1) + min);
@@ -266,6 +234,38 @@ var shop = function() {
             // call shop again to force player to pick a valid option
             shop();
             break;
+    }
+};
+
+// function to end the entire game
+var endGame = function() {
+    window.alert("The game has now ended. Let's see how you did!");
+
+    // check localStorage for high score, if it's not there, use 0
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highscore = 0;
+    }
+    // if player has more monr=ey than the high score, player has new high score!
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+    }
+    else {
+        alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+    }
+
+    // ask player if they'd like to play again
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+
+    if (playAgainConfirm) {
+        // restart the game
+        startGame();
+    }
+    else {
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
     }
 };
 
